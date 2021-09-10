c00e0960671e6301a56644e66104248165fca978
김경민(Kim Kyoungmin)(muktop) <muktop@ncsoft.com>
2021-09-10 21:56:37 +0900

    'save_as_post_print_as_redirect'

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,11 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
 
     if request.method == "POST":
-        return render(request, 'accountapp/content.html', context={'text': 'POST METHOD!'})
+
+        temp = request.POST.get("login_id")
+        new_HelloWorld = HelloWorld()
+        new_HelloWorld.text = temp
+        new_HelloWorld.save()
+
+        hello_world_list = HelloWorld.objects.all
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/content.html', context={'text': 'GET METHOD!'})
+        hello_world_list = HelloWorld.objects.all
+        return render(request, 'accountapp/content.html', context={'hello_world_list': hello_world_list})
