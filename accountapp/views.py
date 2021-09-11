dce2c50afeaacc009a5bed0482847b03ac26f6b5
김경민(Kim Kyoungmin)(muktop) <muktop@ncsoft.com>
2021-09-11 16:15:48 +0900

    login_logout

--- accountapp/views.py
+++ accountapp/views.py
@@ -30,3 +30,8 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+# class LoginView():
+
+# class LogoutView():
+
