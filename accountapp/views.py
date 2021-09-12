3249280f0f18229ea7d0aa68cee4fb83ff6925d8
김경민(Kim Kyoungmin)(muktop) <muktop@ncsoft.com>
2021-09-12 15:15:39 +0900

    'add_detailView'

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import HelloWorld
 
@@ -31,7 +31,12 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
+
 # class LoginView():
 
 # class LogoutView():
 
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
