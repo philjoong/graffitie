8ee6ac4746b5459b3403c40d55be2cca63e06ff5
김경민(Kim Kyoungmin)(muktop) <muktop@ncsoft.com>
2021-09-15 00:10:33 +0900

    add_articleapp

---
+++ articleapp/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+
+from articleapp.models import Article
+
+
+class ArticleCreationForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'image', 'content']
+
+
+class ArticleUpdateForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'image', 'content']
