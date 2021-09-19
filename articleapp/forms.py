8ac24c3c67ff847b2c65edc4bc35e25aafea4953
김경민(Kim Kyoungmin)(muktop) <muktop@ncsoft.com>
2021-09-20 00:08:11 +0900

    add_project_detail_show_all_articles

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -6,7 +6,7 @@ from articleapp.models import Article
 class ArticleCreationForm(ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'image', 'content']
+        fields = ['title', 'image', 'project', 'content']
 
 
 class ArticleUpdateForm(ModelForm):
