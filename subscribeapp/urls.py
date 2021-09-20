e09fac0c7fba081d70b3f8c3057bf61167d4b6b8
김경민(Kim Kyoungmin)(muktop) <muktop@ncsoft.com>
2021-09-20 19:32:13 +0900

    'add_subscribeapp'

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from commentapp.views import CommentCreateView, CommentDeleteView
 from projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListView
-from subscribeapp.views import SubscriptionView
+from subscribeapp.views import SubscriptionView, SubscriptionListView
 
 app_name = "subscribeapp"
 
@@ -10,5 +10,5 @@ urlpatterns = [
     path('subscribe/', SubscriptionView.as_view(), name='subscribe'),
     # path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
     # path('update/<int:pk>', CommentUpdateView.as_view(), name='update'),
-    # path('create/', ProjectCreateView.as_view(), name='create'),
+    path('list/', SubscriptionListView.as_view(), name='list'),
 ]
