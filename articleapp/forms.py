2ef8eb24ba3401967be8191cfabaff61ab46c047
김경민(Kim Kyoungmin)(muktop) <muktop@ncsoft.com>
2021-09-20 20:50:37 +0900

    'add_WYSIWYG'

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,15 +1,22 @@
+from django import forms
 from django.forms import ModelForm
-
+from django import forms
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left'}))
+    project = forms.ModelChoiceField(queryset=Project.objects.all())
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
 
 
+
 class ArticleUpdateForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left'}))
     class Meta:
         model = Article
         fields = ['title', 'image', 'content']
