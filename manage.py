4f86c527cf13615197ebb34cb204d7841d50d2f3
philjoong <muktop@ncsoft.com>
2021-09-22 17:33:13 +0900

    'add_mariadb'

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'graffitie.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'graffitie.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
